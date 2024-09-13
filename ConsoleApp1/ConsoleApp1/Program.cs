14e367667da5939d8aaa315da057a255ca35036c
Svetlana184 <sveta.kgd.39@mail.com>
2024-09-13 21:11:15 +0200

    firs record

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//ввод массива, вывод по строчкам, нахождение максимального элемента
+
+using System
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int a, b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        for (int i = a;  i <= b; i++)
+        {
+            Console.Write(Convert.To.Char(i));
+        }
+
+
+
+    }
+
+}
